8e2abd5b1258ca2828eed9dd7a8a471aa8d0ce19
zairax7u7 <126507734+zairax7u7@users.noreply.github.com>
2023-05-28 23:58:15 -0600

    Add files via upload

---
+++ data/index.js
@@ -0,0 +1,252 @@
+var compp1 = document.getElementById("com1")
+var compp2 = document.getElementById("com2")
+var compp3 = document.getElementById("com3")
+var compp4 = document.getElementById("com4")
+var compp5 = document.getElementById("com5")
+var trabc1t1 = document.getElementById("trab01")
+// ------------------------------------------------
+
+// ------------------------------------------------
+function activecomp1 (){
+  compp1.classList.add("comp1__anim")
+}
+
+function desctivecomp1 (){
+  compp1.classList.remove("comp1__anim")
+}
+// -------------------------------------------------
+function activecomp2 (){
+  compp2.classList.add("comp1__anim")
+}
+
+function desctivecomp2 (){
+  compp2.classList.remove("comp1__anim")
+}
+// -------------------------------------------------
+function activecomp3 (){
+    compp3.classList.add("comp1__anim")
+}
+
+function desctivecomp3 (){
+  compp3.classList.remove("comp1__anim")
+}
+// ------------------------------------------------
+function activecomp4 (){
+  compp4.classList.add("comp1__anim")
+}
+
+function desctivecomp4 (){
+compp4.classList.remove("comp1__anim")
+}
+// ------------------------------------------------
+function activecomp5 (){
+  compp5.classList.add("comp1__anim")
+}
+
+function desctivecomp5 (){
+  compp5.classList.remove("comp1__anim")
+}
+// ------------------------------------------------
+
+function activetrabc1t1(){
+  trabc1t1.style.removeProperty ("transform")
+  trabc1t1.classList.add("gokuanim")
+}
+
+function desctivetrabc1ti(){
+  trabc1t1.classList.remove("gokuanim")
+  trabc1t1.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc1t2 = document.getElementById("trab02")
+
+function activetrabc1t2(){
+  trabc1t2.style.removeProperty ("transform")
+  trabc1t2.classList.add("gokuanim")
+}
+
+function desctivetrabc1t2(){
+  trabc1t2.classList.remove("gokuanim")
+  trabc1t2.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc1t3 = document.getElementById("trab03")
+
+function activetrabc1t3(){
+  trabc1t3.style.removeProperty ("transform")
+  trabc1t3.classList.add("gokuanim")
+}
+
+function desctivetrabc1t3(){
+  trabc1t3.classList.remove("gokuanim")
+  trabc1t3.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc1t4 = document.getElementById("trab04")
+
+function activetrabc1t4(){
+  trabc1t4.style.removeProperty ("transform")
+  trabc1t4.classList.add("gokuanim")
+}
+
+function desctivetrabc1t4(){
+  trabc1t4.classList.remove("gokuanim")
+  trabc1t4.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc2t1 = document.getElementById("trab05")
+
+function activetrabc2t1(){
+  trabc2t1.style.removeProperty ("transform")
+  trabc2t1.classList.add("gokuanim")
+}
+
+function desctivetrabc2t1(){
+  trabc2t1.classList.remove("gokuanim")
+  trabc2t1.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc2t2 = document.getElementById("trab06")
+
+function activetrabc2t2(){
+  trabc2t2.style.removeProperty ("transform")
+  trabc2t2.classList.add("gokuanim")
+}
+
+function desctivetrabc2t2(){
+  trabc2t2.classList.remove("gokuanim")
+  trabc2t2.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc2t3 = document.getElementById("trab07")
+
+function activetrabc2t3(){
+  trabc2t3.style.removeProperty ("transform")
+  trabc2t3.classList.add("gokuanim")
+}
+
+function desctivetrabc2t3(){
+  trabc2t3.classList.remove("gokuanim")
+  trabc2t3.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc3t1 = document.getElementById("trab08")
+
+function activetrabc3t1(){
+  trabc3t1.style.removeProperty ("transform")
+  trabc3t1.classList.add("gokuanim")
+}
+
+function desctivetrabc3t1(){
+  trabc3t1.classList.remove("gokuanim")
+  trabc3t1.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc3t2 = document.getElementById("trab09")
+
+function activetrabc3t2(){
+  trabc3t2.style.removeProperty ("transform")
+  trabc3t2.classList.add("gokuanim")
+}
+
+function desctivetrabc3t2(){
+  trabc3t2.classList.remove("gokuanim")
+  trabc3t2.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc3t3 = document.getElementById("trab010")
+
+function activetrabc3t3(){
+  trabc3t3.style.removeProperty ("transform")
+  trabc3t3.classList.add("gokuanim")
+}
+
+function desctivetrabc3t3(){
+  trabc3t3.classList.remove("gokuanim")
+  trabc3t3.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc3t4 = document.getElementById("trab011")
+
+function activetrabc3t4(){
+  trabc3t4.style.removeProperty ("transform")
+  trabc3t4.classList.add("gokuanim")
+}
+
+function desctivetrabc3t4(){
+  trabc3t4.classList.remove("gokuanim")
+  trabc3t4.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc4t1 = document.getElementById("trab012")
+
+function activetrabc4t1(){
+  trabc4t1.style.removeProperty ("transform")
+  trabc4t1.classList.add("gokuanim")
+}
+
+function desctivetrabc4t1(){
+  trabc4t1.classList.remove("gokuanim")
+  trabc4t1.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc4t2 = document.getElementById("trab013")
+
+function activetrabc4t2(){
+  trabc4t2.style.removeProperty ("transform")
+  trabc4t2.classList.add("gokuanim")
+}
+
+function desctivetrabc4t2(){
+  trabc4t2.classList.remove("gokuanim")
+  trabc4t2.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc4t3 = document.getElementById("trab014")
+
+function activetrabc4t3(){
+  trabc4t3.style.removeProperty ("transform")
+  trabc4t3.classList.add("gokuanim")
+}
+
+function desctivetrabc4t3(){
+  trabc4t3.classList.remove("gokuanim")
+  trabc4t3.style.transform = "scaleX(-1)";
+}
+// *********************************************************c5t1
+var trabc5t1 = document.getElementById("trab015")
+
+function activetrabc5t1(){
+  trabc5t1.style.removeProperty ("transform")
+  trabc5t1.classList.add("gokuanim")
+}
+
+function desctivetrabc5t1(){
+  trabc5t1.classList.remove("gokuanim")
+  trabc5t1.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc5t2 = document.getElementById("trab016")
+
+function activetrabc5t2(){
+  trabc5t2.style.removeProperty ("transform")
+  trabc5t2.classList.add("gokuanim")
+}
+
+function desctivetrabc5t2(){
+  trabc5t2.classList.remove("gokuanim")
+  trabc5t2.style.transform = "scaleX(-1)";
+}
+// *********************************************************
+var trabc5t3 = document.getElementById("trab017")
+
+function activetrabc5t3(){
+  trabc5t3.style.removeProperty ("transform")
+  trabc5t3.classList.add("gokuanim")
+}
+
+function desctivetrabc5t3(){
+  trabc5t3.classList.remove("gokuanim")
+  trabc5t3.style.transform = "scaleX(-1)";
+}
+// *********************************************************
